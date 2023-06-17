84270c5093e92b728979d9ba4b7b2d5c647610fa
HajraZawwar <hajrazawwar@gmail.com>
2023-06-17 18:50:48 +0500

    recyclereview

--- app/src/main/java/com/hajra/hafizrecordapp/recyclerview.java
+++ app/src/main/java/com/hajra/hafizrecordapp/recyclerview.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.BreakIterator;
 import java.util.List;
 
 class myRecyclerViewAdapter extends RecyclerView.Adapter<myRecyclerViewAdapter.MyVH> {
@@ -31,9 +32,10 @@ class myRecyclerViewAdapter extends RecyclerView.Adapter<myRecyclerViewAdapter.M
     @Override
     public void onBindViewHolder(@NonNull myRecyclerViewAdapter.MyVH holder, int position) {
         holder.data=studentList.get(position);
+        holder.textViewFriendID.setText(holder.data.getID());
         holder.textViewFriendName.setText(holder.data.getName());
-        holder.textViewdateFriend.setText(String.valueOf(holder.data.getDob()));
-        holder.textViewCity.setText(holder.data.getCity());
+        holder.textViewdateFriend.setText(String.valueOf(holder.data.getage()));
+        holder.textViewCity.setText(holder.data.getClass1());
     }
 
     @Override
@@ -43,13 +45,17 @@ class myRecyclerViewAdapter extends RecyclerView.Adapter<myRecyclerViewAdapter.M
 
 
     public class MyVH extends RecyclerView.ViewHolder {
-        ImageView imageViewFriend;
+
+        TextView textViewFriendID;
         TextView textViewFriendName;
+
         TextView textViewdateFriend;
         TextView textViewCity;
         student data;
         public MyVH(@NonNull View itemView) {
             super(itemView);
+
+            textViewFriendID= itemView.findViewById(R.id.textViewFriendID);
             textViewFriendName = itemView.findViewById(R.id.textViewFriendName);
             textViewdateFriend = itemView.findViewById(R.id.textViewDate);
             textViewCity = itemView.findViewById(R.id.textViewCity);
